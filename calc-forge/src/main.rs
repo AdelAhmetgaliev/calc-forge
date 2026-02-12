d2b8b4fa248f2a9ebb506b940575821e28908915
Adel Ahmetgaliev <adel.ahmetgaliev@yandex.ru>
2026-02-12 21:17:50 +0300

    Little fix

--- calc-forge/src/main.rs
+++ calc-forge/src/main.rs
@@ -17,8 +17,10 @@ fn main() -> Result<(), Box<dyn Error>> {
     let filename = "data/input_data.dat";
     let data_points = read_data_from_file(filename)?;
 
-    let interp_x: f64 = 0.2;
-    let nearest_points = select_nearest_points(&data_points, 3, interp_x);
+    let interp_x: f64 = -0.2;
+    let poly_degree: usize = 3;
+
+    let nearest_points = select_nearest_points(&data_points, poly_degree, interp_x);
     let interp_y: f64 = lagrange_interp(&nearest_points, interp_x)?;
 
     let (x_values, y_values): (Vec<f64>, Vec<f64>) = data_points.iter().cloned().unzip();
@@ -39,7 +41,7 @@ fn main() -> Result<(), Box<dyn Error>> {
         x_values.clone(),
         y_values_interp,
         NamedColor::Green,
-        "Многочлен Лагранжа 3 степени",
+        "Полином Лагранжа 3 степени",
     );
     let scatter_interp_point = create_scatter_point(
         interp_x,
